f40b1257ac0a585b65f27a0dadac874fad9d108b
baonehe <20520409@gm.uit.edu.vn>
2024-01-02 02:56:44 +0700

    update

--- components/Admin/addscreen_admin.js
+++ components/Admin/addscreen_admin.js
@@ -22,76 +22,13 @@ import {id} from 'date-fns/locale';
 import {KEY} from '@env';
 import CryptoJS from 'react-native-crypto-js';
 import firestore from '@react-native-firebase/firestore';
-const AddScreen_admin = ({navigation}) => {
-  const provinces = [
-    'Hà Nội',
-    'Hồ Chí Minh',
-    'Đà Nẵng',
-    'Hải Phòng',
-    'Cần Thơ',
-    'An Giang',
-    'Bà Rịa-Vũng Tàu',
-    'Bắc Giang',
-    'Bắc Kạn',
-    'Bạc Liêu',
-    'Bắc Ninh',
-    'Bến Tre',
-    'Bình Định',
-    'Bình Dương',
-    'Bình Phước',
-    'Bình Thuận',
-    'Cà Mau',
-    'Cao Bằng',
-    'Đắk Lắk',
-    'Đắk Nông',
-    'Điện Biên',
-    'Đồng Nai',
-    'Đồng Tháp',
-    'Gia Lai',
-    'Hà Giang',
-    'Hà Nam',
-    'Hà Tĩnh',
-    'Hải Dương',
-    'Hậu Giang',
-    'Hòa Bình',
-    'Hưng Yên',
-    'Khánh Hòa',
-    'Kiên Giang',
-    'Kon Tum',
-    'Lai Châu',
-    'Lâm Đồng',
-    'Lạng Sơn',
-    'Lào Cai',
-    'Long An',
-    'Nam Định',
-    'Nghệ An',
-    'Ninh Bình',
-    'Ninh Thuận',
-    'Phú Thọ',
-    'Quảng Bình',
-    'Quảng Nam',
-    'Quảng Ngãi',
-    'Quảng Ninh',
-    'Quảng Trị',
-    'Sóc Trăng',
-    'Sơn La',
-    'Tây Ninh',
-    'Thái Bình',
-    'Thái Nguyên',
-    'Thanh Hóa',
-    'Thừa Thiên-Huế',
-    'Tiền Giang',
-    'Trà Vinh',
-    'Tuyên Quang',
-    'Vĩnh Long',
-    'Vĩnh Phúc',
-    'Yên Bái',
-  ];
+const AddScreen_admin = ({navigation, route}) => {
+  const provinces = route.params;
   const [name, setName] = useState();
   const [location, setLocation] = useState();
   const [policy, setPolicy] = useState();
   const [price, setPrice] = useState();
-  const [province, setProvince] = useState(provinces[0]);
+  const [province, setProvince] = useState(provinces[7]);
   const [slogan, setSlogan] = useState();
   const [detail, setDetail] = useState();
   const [extensions, setExtensions] = useState({
